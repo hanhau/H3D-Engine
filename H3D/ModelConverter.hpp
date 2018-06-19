0c541079412f62fa9b6a2871bd2ef75eb50141a0
Hannes Hauptmann <hanhau.3443@gmail.com>
2018-06-19 11:13:53 +0200

    Update ModelConverter.hpp

--- H3D/ModelConverter.hpp
+++ H3D/ModelConverter.hpp
@@ -1,10 +1,26 @@
+#pragma once 
+#if defined DLL_EXPORT
+#define H3D_API __declspec(dllexport)
+#else
+#define H3D_API __declspec(dllimport)
+#endif
+/////////////////////////////////////////////////////////////////
 #include "FileSystem.hpp"
+#include <string>
 /////////////////////////////////////////////////////////////////
 // Converts any Format to H3D internal Format
 /////////////////////////////////////////////////////////////////
 namespace h3d{
     namespace ModelConverter{
-        bool H3D_API convertToH3DModel();
+        struct Result{
+            bool success;
+            
+            inline Result(){
+                success = false;
+            }
+        };
+        Result H3D_API convertToH3DModel(std::string input_path,
+                                         h3d::Model &md_output);
     }
 }
 /////////////////////////////////////////////////////////////////
