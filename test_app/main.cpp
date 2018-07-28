e8eb6cbd769aecf4259a115af30d7ac989665ab0
Hannes Hauptmann <hannes.hauptmann@outlook.com>
2018-07-28 17:50:14 +0200

    working update

--- test_app/main.cpp
+++ test_app/main.cpp
@@ -3,4 +3,14 @@
 int main()
 {
 	h3d::Window app(h3d::Vec2<int>(800, 600), "Test", h3d::WindowStyle::Default,h3d::ContextSettings());
+	while (app.isOpen())
+	{
+		h3d::Event event;
+		while (app.pollEvent(event)) {
+			if (event.type == h3d::EventType::Closed)
+				app.close();
+
+			
+		}
+	}
 }
